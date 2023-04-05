77f3e5f9d73c554f06e702aff602a8afe093f676
hazratshahjalal <hazratshahjalal503@gmail.com>
2023-04-05 22:32:34 +0600

    bookmark icon added

--- my-react-app/src/Components/Cards/SingleCard.jsx
+++ my-react-app/src/Components/Cards/SingleCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faBookmark } from '@fortawesome/free-solid-svg-icons'
 import './SingleCard.css'
 
 const SingleCard = ({ blog, handleReadTime }) => {
@@ -25,13 +25,13 @@ const SingleCard = ({ blog, handleReadTime }) => {
             </div>
             <div className="readTime">
               <p>{blog.readTime} min to read</p>
-              <FontAwesomeIcon icon={faCoffee} />
+              <FontAwesomeIcon icon={faBookmark} />
             </div>
           </div>
           <div className="blog-title">
             <h2>{blog.blogTitle}</h2>
           </div>
-          <button onClick={() => { handleReadTime(blog.readTime) }} >Mark as read</button>
+          <button id='readButton' onClick={() => { handleReadTime(blog.readTime) }} >Mark as read</button>
         </div>
       </div>
     </div>
